93431881551fbfb747356e67dec3a0b1ced0825c
Marshall Bruce (ACS) <Marshall.Bruce@sanantonio.gov>
2025-02-25 13:48:20 -0600

    Added inv, inv_counts, and locations tables; reworked templates accordingly

--- ims.py
+++ ims.py
@@ -10,47 +10,77 @@ def index():
 
 connect = sqlite3.connect('ims.db')
 
-cur = connect.cursor()
-cur.execute(
-    'CREATE TABLE IF NOT EXISTS ACSINV (name NAME, \
-    division NAME, location NAME, unit_price INTEGER, pref_vendor NAME, \
-    stock_lvl INTEGER, minQty INTEGER, maxQty INTEGER, contract TEXT, \
-    recurring TEXT, qty_need INTEGER, user INTEGER, timestamp DATE)')
+cursor = connect.cursor()
+cursor.execute("PRAGMA foreign_keys = ON")
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS inv (item_id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    item_name VARCHAR(50), item_descr VARCHAR(255), location_id VARCHAR(20), division VARCHAR(20), \
+    min_qty NUMBER, max_qty NUMBER, contract VARCHAR(1), recurring VARCHAR(1))')
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS inv_counts (count_id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    timestamp DATE, user NUMBER, item_id INTEGER,  \
+    stock_lvl NUMBER)'
+)
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS locations (location_id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    facility VARCHAR(14), primary_loc VARCHAR(25), secondary_loc VARCHAR(10), division VARCHAR(20))'
+)
 
 @app.route('/additem', methods=['GET', 'POST'])
 def additem():
     if request.method == 'POST':
-        name = request.form['name']
+        item_name = request.form['item_name']
+        item_descr = request.form['item_descr']
+        location_id = request.form['location_id']
         division = request.form['division']
-        location = request.form['location']
-        stock_lvl = request.form['stock_lvl']
-        minQty = request.form['minQty']
-        maxQty = request.form['maxQty']
-        unit_price = request.form['unit_price']
-        pref_vendor = request.form['pref_vendor']
+        min_qty = request.form['min_qty']
+        max_qty = request.form['max_qty']
         contract = request.form['contract']
         recurring = request.form['recurring']
 
         with sqlite3.connect('ims.db') as users:
             cursor = users.cursor()
-            cursor.execute('INSERT INTO ACSINV \
-                           (name, division, location, unit_price, pref_vendor, stock_lvl, minQty, maxQty, \
-                           contract, recurring) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                           (name, division, location, unit_price, pref_vendor, stock_lvl, minQty, maxQty, 
-                            contract, recurring))
+            cursor.execute('INSERT INTO inv \
+                           (item_name, item_descr, location_id, division, min_qty, max_qty, contract, recurring) \
+                           VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                           (item_name, item_descr, location_id, division, min_qty, max_qty, contract, recurring))
             users.commit()
         return render_template('index.html')
     else:
         return render_template('inventory/additem.html')
+    
+@app.route('/addlocation', methods=['GET', 'POST'])
+def addlocation():
+    if request.method == 'POST':
+        facility = request.form['facility']
+        primary_loc = request.form['primary_loc']
+        secondary_loc = request.form['secondary_loc']
+        division = request.form['division']
+
+        with sqlite3.connect('ims.db') as users:
+            cursor = users.cursor()
+            cursor.execute('INSERT INTO locations \
+                           (facility, primary_loc, secondary_loc, division) \
+                           VALUES (?, ?, ?, ?)',
+                           (facility, primary_loc, secondary_loc, division))
+            users.commit()
+        return render_template('index.html')
+    else:
+        return render_template('inventory/addlocation.html')
     
 @app.route('/viewinventory', methods=['GET'])
 def viewinventory():
     if request.method == 'GET':
         with sqlite3.connect('ims.db') as users:
             cursor = users.cursor()
-            cursor.execute('SELECT * FROM ACSINV ORDER BY DIVISION ASC')
-            data = cursor.fetchall()
-        return render_template('inventory/viewinventory.html', data = data)
+            cursor.execute('SELECT * FROM inv ORDER BY division')
+            inv_data = cursor.fetchall()
+            cursor.execute('SELECT locations.location_id, locations.facility, locations.primary_loc, locations.secondary_loc, inv.location_id \
+                           FROM locations JOIN inv \
+                           ON locations.location_id = inv.location_id \
+                           ')
+            loc_data = cursor.fetchall()
+        return render_template('inventory/viewinventory.html', data = zip(inv_data, loc_data))
     
 @app.route('/needtoorder', methods=['GET'])
 def needtoorder():
@@ -64,62 +94,59 @@ def needtoorder():
             data = cursor.fetchall()
         return render_template('orders/needtoorder.html', data = data)
 
-@app.route('/updatestock', methods=['GET', 'POST'])
-def updatestock():
+@app.route('/updateitem', methods=['GET', 'POST'])
+def updateitem():
     if request.method == 'POST':
-        name = request.form['name']
+        item_name = request.form['item_name']
+        item_descr = request.form['item_descr']
+        location_id = request.form['location_id']
         division = request.form['division']
-        location = request.form['location']
-        stock_lvl = request.form['stock_lvl']
-        minQty = request.form['minQty']
-        maxQty = request.form['maxQty']
-        unit_price = request.form['unit_price']
-        pref_vendor = request.form['pref_vendor']
+        min_qty = request.form['min_qty']
+        max_qty = request.form['max_qty']
         contract = request.form['contract']
         recurring = request.form['recurring']
-        user = request.form['user']
+        item_id = request.form['item_id']
         with sqlite3.connect('ims.db') as users:
             cursor = users.cursor()
-            cursor.execute("UPDATE acsinv SET \
-                           division = ?, location = ?, stock_lvl = ?, minQty = ?, \
-                           maxQty = ?, unit_price = ?, pref_vendor = ?, contract = ?, \
-                           recurring = ?, user = ? WHERE name = ?", 
-                            (division, location, stock_lvl, minQty, maxQty, unit_price,
-                             pref_vendor, contract, recurring, user, name))
+            cursor.execute("UPDATE inv SET \
+                           item_name = ?, item_descr = ?, location_id = ?, division = ?, \
+                           min_qty = ?, max_qty = ?, contract = ?, recurring = ? \
+                           WHERE item_id = ?", 
+                            (item_name, item_descr, location_id, division, min_qty, max_qty,
+                             contract, recurring, item_id))
 
             users.commit()
-            cursor.execute('SELECT * FROM ACSINV ORDER BY DIVISION ASC')
+            cursor.execute('SELECT * FROM inv ORDER BY DIVISION ASC')
 
-            data = cursor.fetchall()    
+            upd_data = cursor.fetchall()    
             
-        return render_template('inventory/updatestock.html', data=data)
+        return render_template('inventory/updateitem.html', upd_data=upd_data)
     else:
         with sqlite3.connect('ims.db') as users:
             cursor = users.cursor()
-            cursor.execute('SELECT name, division, location, stock_lvl, minQty, \
-                           maxQty, unit_price, pref_vendor, contract, recurring \
-                           FROM ACSINV ORDER BY DIVISION ASC')
+            cursor.execute('SELECT * FROM inv')
 
-            data = cursor.fetchall() 
-        return render_template('inventory/updatestock.html', data=data)
+            upd_data = cursor.fetchall() 
+        return render_template('inventory/updateitem.html', upd_data=upd_data)
 
 @app.route('/deleteitem', methods=['GET', 'POST'])
 def deleteitem():
     if request.method == 'POST':
-        name = request.form['name']
+        item_id = request.form['item_id']
         with sqlite3.connect('ims.db') as users:
             cursor = users.cursor()
-            cursor.execute('DELETE FROM ACSINV WHERE name = ?', (name,))
+            cursor.execute('DELETE FROM inv WHERE item_id = ?', (item_id,))
             users.commit()
-            cursor.execute('SELECT * FROM ACSINV ORDER BY DIVISION ASC')
-
+            cursor.execute('SELECT * FROM inv ORDER BY DIVISION')
             data = cursor.fetchall() 
         return render_template('inventory/deleteitem.html', data = data)
     else:
         with sqlite3.connect('ims.db') as users:
             cursor = users.cursor()
-            cursor.execute('SELECT name, division, stock_lvl FROM ACSINV ORDER BY DIVISION ASC')
-
+            cursor.execute('SELECT inv.item_id, inv.item_name, inv.item_descr, inv.division, \
+                           locations.facility, locations.primary_loc, locations.secondary_loc \
+                           FROM inv JOIN locations \
+                           ON inv.location_id = locations.location_id')
             data = cursor.fetchall() 
         return render_template('inventory/deleteitem.html', data = data)
 
